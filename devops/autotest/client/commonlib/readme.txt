91fcf9708ea2f0aa42e68c9e82946da0a910b5b3
Aleksbogdanov12 <119742953+Aleksbogdanov12@users.noreply.github.com>
2023-07-15 18:19:53 +0000

    g

--- devops/autotest/client/commonlib/readme.txt
+++
@@ -1,4 +0,0 @@
-Note:
-Currency rounding formating of Japanese is different from other languages. The minimum unit is 1, no number less than 1.
-
-
--- devops/autotest/service/l10n-service/APITest/src/main/java/com/vmware/singleton/api/rest/RequestMethod.java
+++
@@ -1,7 +0,0 @@
-package com.vmware.singleton.api.rest;
-
-public interface RequestMethod {
-	public static final String POST = "POST";
-	public static final String GET = "GET";
-	public static final String PUT = "PUT";
-}
